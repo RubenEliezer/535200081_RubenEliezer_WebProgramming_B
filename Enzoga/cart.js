8f7ec7a37504425e609de2f3a567b262841e0c41
Ruben Eliezer <89293995+RubenEliezer@users.noreply.github.com>
2021-09-27 10:08:50 +0700

    Add files via upload

---
+++ Enzoga/cart.js
@@ -0,0 +1,6 @@
+function cart-price() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+}
